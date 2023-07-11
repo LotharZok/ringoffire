4bfd500f299186088cd1146c1f1302bed7c2e2ce
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-07-11 09:47:12 +0200

    added card images and start screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: StartScreenComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
