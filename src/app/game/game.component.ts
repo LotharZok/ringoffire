6b2d070209315db78c3464cd56ff564c16cf9ff9
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-07-11 16:32:36 +0200

    Added drawing an displaying of cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -7,22 +7,35 @@ import { Game } from 'src/models/game';
     styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-    pickCardAnimation = false;
+    pickCardAnimation: boolean = false;
+    currentCard: string = '';
     game: Game = new Game();  // auch möglich: Explizite Zuweisung des Typs mit -> game!: Game;
 
     constructor() {}
 
     ngOnInit(): void {
         // this.newGame();
-        console.log(this.game);
     }
 
     newGame() {
         this.game = new Game();
-        console.log(this.game);
     }
 
     pickCard() {
-        this.pickCardAnimation = true;
+        if (!this.pickCardAnimation) {
+            this.currentCard = this.game.stack.pop() as string;
+            // pop() kann auch ein undefined zurückgeben, was currentCard aber nicht annehmen kann. Deshalb das "as string". So kommt auf jeden Fall ein string zurück.
+            this.pickCardAnimation = true;
+            
+            console.log('neue Karte:', this.currentCard);
+            console.log('game-Array', this.game);
+            
+            setTimeout(() => {
+                this.game.playedCards.push(this.currentCard);
+                // Auch hier hätte es einen Fehler gegeben, wenn currentCard ein undefined hätte sein können. Durch die Zeile oben (as string) wird das verhindert.
+                this.pickCardAnimation = false;
+            }, 1000);
+        }
+        
     }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -4,4 +4,29 @@ export class Game {
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
 
+    constructor() {
+        // Karten zum Stack hinzufügen
+        for (let i = 1; i <= 13; i++) {
+            this.stack.push('spades_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+        shuffle(this.stack);
+    }
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a: string[]) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
 }
